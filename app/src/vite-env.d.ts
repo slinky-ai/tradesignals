da4e3444792408f7b39de863b9e384c2be075569
sudomon <mounica551@gmail.com>
2025-04-28 19:59:11 +0400

    updated

---
+++ app/src/vite-env.d.ts
@@ -0,0 +1,16 @@
+
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_APP_ENV: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
+  interface Window {
+    ethereum?: any;
+  }
+  
+  
